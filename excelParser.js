31cfc729b643ec90e1c55f2a5ec78dfbf6759b93
James Nguyen <jameztnguyen@hotmail.com>
2023-12-06 13:10:43 -0800

    parse specific columns in the excel sheets

--- excelParser.js
+++ excelParser.js
@@ -6,7 +6,10 @@ const path = require('path');
 const privateDataExcelFilePath = 'privateData';
 
 // Specify the sheet names you want to parse
-const sheetsToParse = ['SalesOrders', 'Sale Financials'];
+const sheetsToParse = [
+  { sheetName: 'ble_stats', columns: ['toc', 'ttc', 'ttd', 'tic'] },
+  { sheetName: 'sensordata', columns: ['timestamp', 'Display Time'] }
+];
 
 // Read the files in the directory
 fs.readdir(privateDataExcelFilePath, (err, files) => {
@@ -26,15 +29,29 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
     const privateDataExcel = XLSX.readFile(excelFilePath);
 
     // Iterate through each sheet
-    sheetsToParse.forEach(sheetName => {
+    sheetsToParse.forEach(sheetInfo => {
+      const { sheetName, columns } = sheetInfo;
 
       // Check if sheet exists in workbook
       if(privateDataExcel.Sheets.hasOwnProperty(sheetName)) {
         // Get the data from the sheet
-        const sheetData = XLSX.utils.sheet_to_json(privateDataExcel.Sheets[sheetName]);
+        const sheetData = XLSX.utils.sheet_to_json(privateDataExcel.Sheets[sheetName], { header: 1 });
+
+        // Find column indices in the header row
+        const headerRow = sheetData[0];
+
+        // Extract specific columns from each row
+        const parsedData = sheetData.slice(1).map(row => {
+          const rowData = {};
+          columns.forEach(col => {
+            const columnIndex = headerRow.indexOf(col);
+            rowData[col] = row[columnIndex];
+          });
+          return rowData;
+        });
 
         // Now 'sheetData' contains an array of objects representing the data in the current sheet
-        console.log(`Data from sheet "${sheetName}" in file "${excelFile}":`, sheetData);
+        console.log(`Data from sheet "${sheetName}" in file "${excelFile}":`, parsedData);
       }
       else {
         console.log(`Sheet "${sheetName}" not found in file "${excelFile}".`);
