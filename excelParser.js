20dfc3feb6d118ed5296550c3b43f9ef12a1b15f
James Nguyen <jameztnguyen@hotmail.com>
2023-12-05 20:45:55 -0800

    read generic folder files

--- excelParser.js
+++ excelParser.js
@@ -1,12 +1,34 @@
 const XLSX = require('xlsx');
 const fs = require('fs');
+const path = require('path');
 
-const privateDataExcelFilePath = 'privateData\\SampleData.xlsx';
+// Specify the directory path
+const privateDataExcelFilePath = 'privateData';
 
-const privateDataExcel = XLSX.readFile(privateDataExcelFilePath);
+// Read the files in the directory
+fs.readdir(privateDataExcelFilePath, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
 
-privateDataExcel.SheetNames.forEach(sheetName => {
-  const sheetData = XLSX.utils.sheet_to_json(privateDataExcel.Sheets[sheetName]);
+  // Filter Excel files
+  const excelFiles = files.filter(file => path.extname(file).toLowerCase() === '.xlsx');
 
-  console.log(`Dta from sheet "{sheetName}":`, sheetData);
-})
+  // Iterate through each Excel file
+  excelFiles.forEach(excelFile => {
+    const excelFilePath = path.join(privateDataExcelFilePath, excelFile);
+
+    // Read the Excel file
+    const privateDataExcel = XLSX.readFile(excelFilePath);
+
+    // Iterate through each sheet
+    privateDataExcel.SheetNames.forEach(sheetName => {
+      // Get the data from the sheet
+      const sheetData = XLSX.utils.sheet_to_json(privateDataExcel.Sheets[sheetName]);
+
+      // Now 'sheetData' contains an array of objects representing the data in the current sheet
+      console.log(`Data from sheet "${sheetName}" in file "${excelFile}":`, sheetData);
+    });
+  });
+});
