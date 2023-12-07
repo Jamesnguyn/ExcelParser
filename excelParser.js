66f1684bdb3f2b17f791e863ed07afc1069afc43
James Nguyen <jameztnguyen@hotmail.com>
2023-12-06 22:18:35 -0800

    debug commit

--- excelParser.js
+++ excelParser.js
@@ -11,6 +11,11 @@ const sheetsToParse = [
   { sheetName: 'sensordata', columns: ['timestamp', 'Display Time'] }
 ];
 
+let sensorDataFirstTimestamp;
+let sensorDataLastTimestamp;
+let lowerBound;
+let upperBound;
+
 // Read the files in the directory
 fs.readdir(privateDataExcelFilePath, (err, files) => {
   if (err) {
@@ -38,7 +43,7 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
     const firstFormattedDate = `${firstDatePart.slice(0, 2)}/${firstDatePart.slice(2, 4)}/${firstDatePart.slice(4)}`;
     const firstTimePart = firstDateAndTime[1];
     const firstFormattedTime = `${firstTimePart.slice(0, 2)}:${firstTimePart.slice(2)}:00`;
-    const lowerBound = firstFormattedDate.concat(".", firstFormattedTime);
+    lowerBound = firstFormattedDate.concat(".", firstFormattedTime);
 
     // find upper bound date and time
     const secondPart = fileNameParts.slice(2).join('.');
@@ -47,7 +52,7 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
     const secondFormattedDate = `${secondDatePart.slice(0, 2)}/${secondDatePart.slice(2, 4)}/${secondDatePart.slice(4)}`;
     const secondTimePart = secondDateAndTime[1];
     const secondFormattedTime = `${secondTimePart.slice(0, 2)}:${secondTimePart.slice(2)}:00`;
-    const upperBound = secondFormattedDate.concat(".", secondFormattedTime);
+    upperBound = secondFormattedDate.concat(".", secondFormattedTime);
 
     // Read the Excel file
     const privateDataExcel = XLSX.readFile(excelFilePath);
@@ -57,7 +62,7 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
       const { sheetName, columns } = sheetInfo;
 
       // Check if sheet exists in workbook
-      if(privateDataExcel.Sheets.hasOwnProperty(sheetName)) {
+      if (privateDataExcel.Sheets.hasOwnProperty(sheetName)) {
         // Get the data from the sheet
         const sheetData = XLSX.utils.sheet_to_json(privateDataExcel.Sheets[sheetName], { header: 1 });
 
@@ -69,7 +74,7 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
           const rowData = {};
           columns.forEach(col => {
             const columnIndex = headerRow.indexOf(col);
-	
+
             // Check if the current column is "Display Time"
             // Now 'sheetData' contains an array of objects representing the data in the current sheet
             if (col === "Display Time") {
@@ -84,7 +89,6 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
           return rowData;
         });
 
-        	
         // Filter data between lowerBound and upperBound
         const filteredData = parsedData.filter(row => {
           const rowDisplayTime = row["Display Time"];
@@ -95,18 +99,60 @@ fs.readdir(privateDataExcelFilePath, (err, files) => {
         if (sheetName === "sensordata") {
           if (filteredData.length > 0) {
             const timestampValues = filteredData.map(row => row["timestamp"]);
-            console.log('First Timestamp Value:', timestampValues[0]);
-            console.log('Last Timestamp Value:', timestampValues[filteredData.length - 1]);
+            sensorDataFirstTimestamp = timestampValues[0];
+            sensorDataLastTimestamp = timestampValues[timestampValues.length - 1];
+            console.log("First TimeStamp: ", sensorDataFirstTimestamp);
+            console.log("Last TimeStamp: ", sensorDataLastTimestamp);
+
+            // You can also print lowerBound and upperBound for debugging
+            console.log("Lower Bound: ", lowerBound);
+            console.log("Upper Bound: ", upperBound);
           }
         }
-        // Now 'sheetData' contains an array of objects representing the data in the current sheet
-        // console.log(lowerBound);
-        // console.log(upperBound);
-        // console.log(`Data from sheet "${sheetName}" in file "${excelFile}":`, filteredData);
-        // console.log(`Number of items in sheet "${sheetName}" in file "${excelFile}":`, filteredData.length);
-      }
-      else {
-        console.log(`Sheet "${sheetName}" not found in file "${excelFile}".`);
+
+        if (sheetName === "ble_stats") {
+          // Log column names in "ble_stats"
+          // console.log(`Column names in "ble_stats":`, headerRow);
+          
+          // Check if the "timestamp" column is present
+          const timestampColumnName = "toc"; // Change this to the actual column name
+          if (!headerRow.includes(timestampColumnName)) {
+            console.error(`Error: "${timestampColumnName}" column not found in sheet "${sheetName}" in file "${excelFile}".`);
+            return;
+          }
+
+          const bleStatsData = sheetData.slice(1).map(row => {
+            const rowData = {};
+            columns.forEach(col => {
+              const columnIndex = headerRow.indexOf(col);
+              rowData[col] = row[columnIndex];
+            });
+            return rowData;
+          });
+          
+          // Log the timestamp values in "ble_stats" for debugging
+          // const bleStatsTimestamps = bleStatsData.map(row => row[timestampColumnName]);
+          // console.log(`Timestamp values in "ble_stats":`, bleStatsTimestamps);
+
+          // Filter data in "ble_stats" between sensorDataFirstTimestamp and sensorDataLastTimestamp
+          const filteredBleStatsData = bleStatsData.filter(row => {
+            const rowTimestamp = row[timestampColumnName];
+            console.log(`Row timestamp: ${rowTimestamp}, Lower Bound: ${sensorDataFirstTimestamp}, Upper Bound: ${sensorDataLastTimestamp}`);
+            return rowTimestamp >= sensorDataFirstTimestamp && rowTimestamp <= sensorDataLastTimestamp;
+          });
+
+          console.log(`Filtered data from sheet "${sheetName}" between first and last timestamp of "sensordata":`, filteredBleStatsData);
+
+          // Print all data from "ble_stats" for debugging
+          console.log(`All data from sheet "${sheetName}" in file "${excelFile}":`, bleStatsData);
+          // console.log(`Data from sheet "${sheetName}" in file "${excelFile}":`, filteredData);
+          console.log(`Number of items in sheet "${sheetName}" in file "${excelFile}":`, filteredData.length);
+        
+          
+        }
+        else {
+          console.log(`Sheet "${sheetName}" not found in file "${excelFile}".`);
+        }
       }
     });
   });
