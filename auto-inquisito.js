238e508ce0bbaabe68f2ec98c65d182dbdf54dc4
James Nguyen <jameztnguyen@hotmail.com>
2024-01-17 10:35:06 -0800

    input login info

--- auto-inquisito.js
+++ auto-inquisito.js
@@ -10,10 +10,32 @@ const puppeteer = require('puppeteer');
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
   await page.goto('https://prod-vnv-inquisito-ui.dexcomdev.com');
-  // await page.goto('https://youtube.com');
 
-  // Wait for navigation to complete
-  await page.waitForNavigation();
+  await page.waitForSelector('.login-button-container');
+  await page.click('.login-button-container');
+
+  // Wait for the username input field to appear
+  await page.waitForSelector('input[name="identifier"]');
+  // Type into the username input field
+  await page.type('input[name="identifier"]', 'test@dexcom.com');
+
+  // Wait for the password input field to appear
+  await page.waitForSelector('input[name="credentials.passcode"]');
+  // Type into the password input field
+  await page.type('input[name="credentials.passcode"]', 'Hello, Puppeteer!');
+
+  await page.click('span.eyeicon.visibility-16.button-show');
+
+  // Wait for the submit button to appear
+  await page.waitForSelector('input.button.button-primary[type="submit"][value="Sign in"][data-type="save"]');
+
+  await page.evaluate(() => {
+    document.querySelector('input.button.button-primary[type="submit"][value="Sign in"][data-type="save"]').click();
+  });
+
+  // await page.click('input.button.button-primary[type="submit"][value="Sign in"][data-type="save"]');
+
+  new Promise(r => setTimeout(r, 3000));
 
   await page.screenshot({ path: 'example.png' });
 
