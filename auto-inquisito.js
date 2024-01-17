1e9548538802c83adae6205cc8fd87f67c09b044
James Nguyen <jameztnguyen@hotmail.com>
2024-01-17 08:41:05 -0800

    access inquisito website

--- auto-inquisito.js
+++ auto-inquisito.js
@@ -6,7 +6,15 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
 
+  // Set a custom user agent
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
   await page.goto('https://prod-vnv-inquisito-ui.dexcomdev.com');
+  // await page.goto('https://youtube.com');
+
+  // Wait for navigation to complete
+  await page.waitForNavigation();
+
   await page.screenshot({ path: 'example.png' });
 
   await browser.close();
